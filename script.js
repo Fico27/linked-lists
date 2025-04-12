f688999d321450f8ddffc094041217e30a71a757
Phil <182430206+Fico27@users.noreply.github.com>
2025-04-12 13:15:20 +0200

    working on printing node lists

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-class LinkedList {}
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  printList() {
+    let node = this.head;
+    while (node) {
+      console.log(`${node.value}`);
+      node = node.nextNode;
+    }
+  }
+}
 
 class Node {
-  constructor(value) {
-    this.value = null;
+  constructor(value = null) {
+    this.value = value;
     this.nextNode = null;
   }
 }
