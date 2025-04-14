9517704c3401e712e1ded814b32371f68fc01f77
Phil <182430206+Fico27@users.noreply.github.com>
2025-04-14 13:50:01 +0200

    added in the ability to append, prepend, return size, return head, return tail, return a node at an index.

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
 class LinkedList {
   constructor(head = null) {
     this.head = head;
+    this.tail = null;
+    this.size = 0;
+    this.lastNode = null;
   }
 
-  printList() {
-    let node = this.head;
-    while (node) {
-      console.log(`${node.value}`);
-      node = node.nextNode;
+  append(value) {
+    if (this.head === null) {
+      this.head = new Node(value);
+      this.size++;
+    } else if (this.head.nextNode) {
+      let node = new Node(value);
+      this.lastNode.nextNode = node;
+      this.lastNode = node;
+      this.tail = node;
+      this.size++;
+    } else {
+      let node = new Node(value);
+      this.tail = node;
+      this.head.nextNode = node;
+      this.lastNode = node;
+      this.size++;
     }
+  }
+
+  prepend(value) {
+    let holdThis = this.head.nextNode;
+    let node = new Node(value);
+    this.head.nextNode = node;
+    node.nextNode = holdThis;
+    this.size++;
+  }
+
+  size() {
+    return this.size;
+  }
+
+  giveHead() {
+    return this.head.value;
+  }
+
+  giveTail() {
+    return this.lastNode.value;
+  }
+
+  at(index) {
+    let currentNode = this.head;
+    let counter = 0;
+
+    while (counter !== index) {
+      currentNode = currentNode.nextNode;
+      counter++;
+    }
+    return currentNode;
   }
 }
 
